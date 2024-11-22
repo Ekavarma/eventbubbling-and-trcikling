682da46346e7db2f2d1bbce265b53f1dcf587a13
Ekavarma Petla <ekavarmap@gmail.com>
2024-11-22 23:46:46 +0530

    event bubbling and event trickling

---
+++ js.js
@@ -0,0 +1,117 @@
+// const Gparent = document.getElementById("grand")
+// Gparent.addEventListener("click",()=>{
+//     Gparent.style.backgroundColor="green"
+//     console.log("grandparent")
+// })
+
+// const parent1 = document.getElementById("parent")
+// parent1.addEventListener("click",()=>{
+//     parent1.style.backgroundColor="green"
+//     console.log("parent")
+// })
+
+// const child1 = document.getElementById("child")
+// child1.addEventListener("click",(e)=>{
+
+//     child1.style.backgroundColor="lightred"
+//     console.log("child")
+// })
+
+
+
+//setTimeout(()=>{})
+
+// const Gparent = document.getElementById("grand")
+// Gparent.addEventListener("click",()=>{
+//     setTimeout(()=>{
+//         Gparent.style.backgroundColor="green"
+//     console.log("grandparent")
+//     })
+    
+// })
+
+// const parent1 = document.getElementById("parent")
+// parent1.addEventListener("click",()=>{
+//     setTimeout(()=>{
+//         parent1.style.backgroundColor="pink"
+//     console.log("parent")
+//     })
+    
+// })
+
+// const child1 = document.getElementById("child")
+// child1.addEventListener("click",(e)=>{
+//     setTimeout(()=>{
+//         child1.style.backgroundColor="blue"
+//     console.log("child")
+//     })
+
+    
+// })
+
+// const Gparent = document.getElementById("grand")
+// Gparent.addEventListener("click",(e)=>{
+//     e.stopPropagation()
+//     setTimeout(()=>{
+//         Gparent.style.backgroundColor="red"
+//     console.log("grandparent")
+//     })
+    
+// })
+
+// const parent1 = document.getElementById("parent")
+// parent1.addEventListener("click",(e)=>{
+//     e.stopPropagation()
+
+//     setTimeout(()=>{
+//         parent1.style.backgroundColor="green"
+//     console.log("parent")
+//     })
+    
+// })
+
+// const child1 = document.getElementById("child")
+// child1.addEventListener("click",(e)=>{
+//     e.stopPropagation()
+
+//     setTimeout(()=>{
+//         child1.style.backgroundColor="pink"
+//     console.log("child")
+//     })
+
+    
+// })
+
+
+// const Gparent = document.getElementById("grand")
+// Gparent.addEventListener("click",(e)=>{
+//     // e.stopPropagation()
+//     setTimeout(()=>{
+//         Gparent.style.backgroundColor="red"
+//     console.log("grandparent")
+//     })
+    
+// },true)
+
+// const parent1 = document.getElementById("parent")
+// parent1.addEventListener("click",(e)=>{
+//     // e.stopPropagation()
+
+//     setTimeout(()=>{
+//         parent1.style.backgroundColor="green"
+//     console.log("parent")
+//     })
+    
+// },true)
+
+// const child1 = document.getElementById("child")
+// child1.addEventListener("click",(e)=>{
+//     // e.stopPropagation()
+
+//     setTimeout(()=>{
+//         child1.style.backgroundColor="pink"
+//     console.log("child")
+//     })
+
+    
+// },true)
